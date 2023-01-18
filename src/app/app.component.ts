b138c20f2c0fca1cf09ea5f84307a078e05df1ad
dhairyaj99 <dj17vi@brocku.ca>
2023-01-17 19:07:46 -0500

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Platform } from '@angular/cdk/platform';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+import { FormControl } from '@angular/forms';
+import { DateAdapter, MAT_DATE_LOCALE, NativeDateAdapter } from '@angular/material/core';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
+
+export class MonthpickerDateAdapter extends NativeDateAdapter {
+  constructor(matDateLocale: string, platform: Platform) {
+    super(matDateLocale, platform);
+  }
+
+  override format(date: Date, displayFormat: any): string {
+    const month = date.getMonth() + 1;
+    const monthAsString = ('0' + month).slice(-2);
+    const day = date.getDate();
+    const dayAsString = ('0' + day).slice(-2);
+    return dayAsString + monthAsString;
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [
+    {
+      provide: DateAdapter,
+      useClass: MonthpickerDateAdapter,
+      deps: [MAT_DATE_LOCALE, Platform],
+    },
+  ]
 })
+
 export class AppComponent {
   title = 'datepicker';
+  @Input()
+  public monthAndYear: Date | null = null;
+
+  @Output()
+  public monthAndYearChange = new EventEmitter<Date | null>();
+
+  public emitDateChange(event: MatDatepickerInputEvent<Date | null, unknown>): void {
+    this.monthAndYearChange.emit(event.value);
+  }
+
+  public monthChanged(value: any, widget: any): void {
+    this.monthAndYear = value;
+    widget.close();
+  }
+
+  
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MaterialExampleModule} from '../material.module';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +18,18 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule,
+    MaterialExampleModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
+  entryComponents: [AppComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
